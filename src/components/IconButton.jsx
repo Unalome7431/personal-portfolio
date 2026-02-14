a0b933624cec1de43fecdb9aa4001e12874900f5
Velengio Deriksen Charles <velengio@gmail.com>
2026-02-14 13:58:32 +0700

    another changes

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -4,7 +4,7 @@ function IconButton({ Icon, onClickFunction }) {
     <div className="relative inline-block">
       <div class="absolute inset-0 bg-emerald-700 translate-0.5 translate-y-0.5 rounded-2xl z-0"></div> 
 
-      <button className="relative z-1 bg-neutral-200 p-1.5 border-2 border-emerald-700 rounded-2xl text-emerald-800 hover:text-emerald-600 translate-0.5 translate-y-0.5 hover:-translate-0.5 hover:-translate-y-0.5 transition-transform duration-300 ease-out active:translate-0.5 active:translate-y-0.5">
+      <button className="relative z-1 bg-neutral-200 p-1.5 border-2 border-emerald-700 rounded-2xl text-emerald-700 hover:text-emerald-600 translate-0.5 translate-y-0.5 hover:-translate-0.5 hover:-translate-y-0.5 transition-transform duration-300 ease-out active:translate-0.5 active:translate-y-0.5">
         <Icon className="transition-colors duration-100 ease-in"/>
       </button> 
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,26 +3,26 @@ function NavBar() {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 h-20 border-b shadow-md bg-[#F8F6F4]">
       <div className="flex gap-3 justify-between mr-7 ml-7 mt-7">
-        <span className="text-emerald-600 font-medium text-2xl">{"<"}Velengio{"/>"}</span>
+        <span className="text-emerald-700 font-medium text-2xl">{"<"}Velengio{"/>"}</span>
         <div className="flex gap-3">
-          <span className="text-emerald-600 font-medium">1.</span>
-          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-600 hover:decoration-3 transition-all duration-300 ease-linear">
+          <span className="text-emerald-700 font-medium">1.</span>
+          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-700 hover:decoration-3 transition-all duration-300 ease-linear">
             About
           </span>
-          <span className="text-emerald-600 font-medium">2.</span>
-          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-600 hover:decoration-3 transition-all duration-300 ease-linear">
+          <span className="text-emerald-700 font-medium">2.</span>
+          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-700 hover:decoration-3 transition-all duration-300 ease-linear">
             Education
           </span>
-          <span className="text-emerald-600 font-medium">3.</span>
-          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-600 hover:decoration-3 transition-all duration-300 ease-linear">
+          <span className="text-emerald-700 font-medium">3.</span>
+          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-700 hover:decoration-3 transition-all duration-300 ease-linear">
             Experience
           </span>
-          <span className="text-emerald-600 font-medium">4.</span>
-          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-600 hover:decoration-3 transition-all duration-300 ease-linear">
+          <span className="text-emerald-700 font-medium">4.</span>
+          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-700 hover:decoration-3 transition-all duration-300 ease-linear">
             Work
           </span>
-          <span className="text-emerald-600 font-medium">5.</span>
-          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-600 hover:decoration-3 transition-all duration-300 ease-linear">
+          <span className="text-emerald-700 font-medium">5.</span>
+          <span className="hover:underline hover:underline-offset-4 hover:decoration-emerald-700 hover:decoration-3 transition-all duration-300 ease-linear">
             Contact
           </span>
         </div>
