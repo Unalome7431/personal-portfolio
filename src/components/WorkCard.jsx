4f01001a6cce2f92a161763b767ff31ef135780f
Velengio Deriksen Charles <velengio@gmail.com>
2026-02-15 16:22:16 +0700

    added work nav

---
+++ src/components/WorkCard.jsx
@@ -0,0 +1,30 @@
+import IconButton from "./IconButton"
+import { SquareArrowOutUpRight, Github } from "lucide-react"
+import ProfileImage from '../assets/profile-placeholder.jpg'
+import { useState } from "react"
+
+function WorkCard({ title, desc, onGithubClick, onLinkClick, imgSrc }) {
+  const [isHover, setIsHover] = useState(false)
+
+  return (
+    <div onMouseEnter={() => {setIsHover(true)}} onMouseLeave={() => {setIsHover(false)}} className="relative w-80 bg-white rounded-2xl mb-10">
+      <div className="overflow-hidden rounded-tl-2xl rounded-tr-2xl">
+        <img className={`${isHover ? 'scale-105' : ''} transition-all duration-300 ease-in-out`} src={ProfileImage} alt="" />
+      </div>
+
+      <div className="p-3 flex flex-col gap-2">
+        <div>
+          <h3 className="font-medium text-emerald-700">{title}</h3>
+          <p>{desc}</p>
+        </div>
+
+        <div className="flex gap-2">
+          <IconButton Icon={Github} onClickFunction={onGithubClick} />
+          <IconButton Icon={SquareArrowOutUpRight} onClickFunction={onLinkClick} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WorkCard
--- src/navigation/Work.jsx
+++ src/navigation/Work.jsx
@@ -1,8 +1,20 @@
+import WorkCard from "../components/WorkCard"
+
 function Work() {
 
   return (
     <div className="h-fit bg-[#F6F6F6] pl-2 pr-2">
-      <p className="text-neutral-500 text-xl ml-22 mb-5 pt-10">{"<Work />"}</p>
+      <p className="text-neutral-500 text-xl ml-22 mb-5 pt-50">{"<Work />"}</p>
+
+      <div className="ml-22 grid grid-cols-4">
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+        <WorkCard title={"Sui"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda!"} />
+      </div>
     </div>
   )
 }
